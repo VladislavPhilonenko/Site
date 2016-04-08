5d62de1fe2df619343c75b0d0602a14c52a30461
Vlad Filonenko <filonenkovladislav@mail.ru>
2016-04-08 17:05:14 +0300

    first commit

---
+++ custom.js
@@ -0,0 +1,11 @@
+     	document.getElementsByClassName("pow")[0].addEventListener("mouseover", changeColor);
+		document.getElementsByClassName("pow")[0].addEventListener("mouseover", changeColor2);
+		
+		function changeColor() {
+			document.getElementsByClassName("pow").style.backgroundColor = "red";
+		};
+		function changeColor2() {
+			document.getElementsByClassName("con")[0].style.backgroundColor = "white";
+		};
+		
+		console.log("Hello!!");	
